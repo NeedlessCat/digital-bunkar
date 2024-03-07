9c599afb634ed19281429b2e294d582fc2fb8174
NeedlessCat <kumar29.aayush@gmail.com>
2024-03-07 06:02:40 +0530

    added self img

--- src/components/Home/ToolSlider/SliderData.js
+++ src/components/Home/ToolSlider/SliderData.js
@@ -1,8 +1,8 @@
-import img1 from "../../../assets/1.jpg";
-import img2 from "../../../assets/2.jpg";
-import img3 from "../../../assets/3.jpg";
-import img4 from "../../../assets/4.jpg";
-import img5 from "../../../assets/5.jpg";
+import img1 from "../../../assets/tool1.jpg";
+import img2 from "../../../assets/tool2.jpg";
+import img3 from "../../../assets/tool3.jpg";
+import img4 from "../../../assets/tool4.jpeg";
+import img5 from "../../../assets/tool5.jpg";
 
 const images = [
   {
